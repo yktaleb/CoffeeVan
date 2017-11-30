1484aa87512020a87bb18b968fa694e2d393c7a2
yktaleb <taleb.yarik@gmail.com>
2017-11-30 03:13:54 +0200

    Added main methods to the beverage order dao

--- src/main/java/ua/training/dao/impl/BeverageOrderDaoImpl.java
+++ src/main/java/ua/training/dao/impl/BeverageOrderDaoImpl.java
@@ -3,8 +3,11 @@ package ua.training.dao.impl;
 import ua.training.dao.AbstractDao;
 import ua.training.dao.BeverageOrderDao;
 import ua.training.dao.BeverageQualityDao;
+import ua.training.dao.factory.MySqlDaoFactory;
+import ua.training.entity.Beverage;
 import ua.training.entity.BeverageOrder;
 import ua.training.entity.BeverageQuality;
+import ua.training.entity.Order;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -13,21 +16,53 @@ import java.sql.SQLException;
 import java.util.Optional;
 
 public class BeverageOrderDaoImpl extends AbstractDao<BeverageOrder> implements BeverageOrderDao {
-    public BeverageOrderDaoImpl(String tableName, Connection connection) {
-        super(tableName, connection);
+    private static final String TABLE_NAME = "beverage_order";
+    private static final String ID = "id";
+    private static final String ORDER = "order";
+    private static final String BEVERAGE = "beverage";
+    private static final String AMOUNT = "amount";
+
+    private BeverageOrderDaoImpl(Connection connection) {
+        super(TABLE_NAME, connection);
+    }
+
+    private static final class BeverageOrderDaoImplHolder {
+        private static BeverageOrderDaoImpl instance(Connection connection) {
+            return new BeverageOrderDaoImpl(connection);
+        }
+    }
+
+    public static BeverageOrderDaoImpl getInstance(Connection connection) {
+        return BeverageOrderDaoImplHolder.instance(connection);
     }
 
     @Override
     protected String[] getParameterNames() {
-        return new String[0];
+        return new String[]{ORDER, BEVERAGE, AMOUNT};
     }
 
     @Override
-    protected void setEntityParameters(BeverageOrder entity, PreparedStatement statement) {
+    protected void setEntityParameters(BeverageOrder beverageOrder, PreparedStatement statement) throws SQLException {
+        statement.setLong(1, beverageOrder.getOrder().getId());
+        statement.setLong(2, beverageOrder.getBeverage().getId());
+        statement.setLong(3, beverageOrder.getAmount());
     }
 
     @Override
     protected Optional<BeverageOrder> getEntityFromResultSet(ResultSet resultSet) throws SQLException {
-        return null;
+        long id = resultSet.getLong(ID);
+        long orderId = resultSet.getLong(ORDER);
+        long beverageId = resultSet.getLong(BEVERAGE);
+        int amount = resultSet.getInt(AMOUNT);
+        Order order = MySqlDaoFactory.getInstance(connection).createOrderDao().findOne(orderId).get();
+        Beverage beverage = MySqlDaoFactory.getInstance(connection).createBeverageDao().findOne(beverageId).get();
+        return Optional.of(
+                new BeverageOrder.BeverageOrderBuilder()
+                        .setId(id)
+                        .setOrder(order)
+                        .setBeverage(beverage)
+                        .setAmount(amount)
+                        .build()
+        );
     }
 }
